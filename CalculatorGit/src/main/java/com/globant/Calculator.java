f37dd5f674e5b5589de0e4c1fc00a21fe74be84c
Lina Maria Beltran <limbletrana@gmail.com>
2021-01-21 14:50:52 -0500

    add Method for sum an subtract

---
+++ CalculatorGit/src/main/java/com/globant/Calculator.java
@@ -0,0 +1,9 @@
+package src.main.java.com.globant;
+
+public abstract class Calculator {
+	double number1;
+	double number2;
+	double result;
+	abstract void operation();
+
+}
---
+++ CalculatorGit/src/main/java/com/globant/Subtraction.java
@@ -0,0 +1,15 @@
+package src.main.java.com.globant;
+
+public class Subtraction extends Calculator {
+
+	public Subtraction(double number1, double number2) {
+		this.number1 = number1;
+		this.number2 = number2;
+	}
+	
+	public void operation() {
+		double result = number1 - number2;
+		System.out.println("Este es el resultado de la sustracción:" + result);
+	}
+
+}
